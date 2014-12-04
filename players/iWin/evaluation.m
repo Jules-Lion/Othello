bb1abcb24f3952992c6b04540aaf1f5d6219e5c2
imen-ih <imen_houissa@hotmail.de>
2014-12-04 13:51:45 +0100

    Implementierung der Steindifferenz, RandSteine, corners, corner closeness, mobility (auskommentiert).

--- players/iWin/evaluation.m
+++ players/iWin/evaluation.m
@@ -1,4 +1,4 @@
-function [value] = evaluation(board, color, moves_list)
+function score = evaluation(board, color, moves_list)
 
 %% Initialize valueTable
     valueTable = [  99,  -8,  8,  6,  6,  8,  -8, 99;
@@ -9,36 +9,15 @@ function [value] = evaluation(board, color, moves_list)
                      8,  -4,  7,  4,  4,  7,  -4,  8;
                     -8, -24, -4, -3, -3, -4, -24, -8;
                     99,  -8,  8,  6,  6,  8,  -8, 99];
-                
+    
+    %Richtungen definieren
+    Rx = [-1, -1, 0, 1, 1, 1, 0, -1];
+    Ry = [0, 1, 1, 1, 0, -1, -1, -1];
     value = 0;
     eigSteine = 0;
     gegSteine = 0;
-                         
-% %% Stabile Felder
-% 
-%     if(board(1,1)==color)
-% 		valueTable(1,2)=8;
-% 		valueTable(2,1)=8;
-% 		valueTable(2,2)=7;
-% 	end
-% 
-% 	if(board(1,8)==color)
-% 		valueTable(1,7)=8;
-% 		valueTable(2,8)=8;
-% 		valueTable(2,7)=7;
-% 	end
-% 
-% 	if(board(8,1)==color)
-% 		valueTable(7,1)=8;
-% 		valueTable(8,2)=8;
-% 		valueTable(7,2)=7;
-% 	end
-% 
-% 	if(board(8,8)==color)
-% 		valueTable(7,8)=8;
-% 		valueTable(8,7)=8;
-% 		valueTable(7,7)=7;
-%     end
+    eigRandSteine = 0;
+    gegRandSteine = 0;
 
 %% Final evaluation
 
@@ -52,8 +31,156 @@ function [value] = evaluation(board, color, moves_list)
                 gegSteine = gegSteine +1;
             end 
             
+            if (board(i,j) ~= 0)  % stabilitätskriterium sind die randsteine.
+                for k=1:8
+                    x = i + Rx(k);
+                    y = j + Ry(k);
+                    if ((x >= 1) && (x <= 8) && (y >= 1) && (y <= 8) && (board(x,y)==0))
+                        if (board(i,j) == color)
+                            eigRandSteine = eigRandSteine +1;
+                        else
+                            gegRandSteine = gegRandSteine +1;
+                        
+                        end % if (board(i,j) == color)
+                        break;
+                    end % if
+                end % for k=1:8
+            end % if (board(i,j) ~= 0)
+        end % for j=1:8
+    end %for i=1:8
+ 
+    % Steindifferenz berechnen
+    if(eigSteine > gegSteine)
+		piece_diff = (100.0 * eigSteine)/(eigSteine + gegSteine);
+    elseif(eigSteine < gegSteine)
+		piece_diff = -(100.0 * gegSteine)/(eigSteine + gegSteine);
+	else piece_diff = 0;
+    end % if (eigSteine > gegSteine)
 
-                
-        end
+    % Randsteine berechnen
+	if(eigRandSteine > gegRandSteine)
+		rand_Steine = -(100.0 * eigRandSteine)/(eigRandSteine + gegRandSteine);
+    elseif(eigRandSteine < gegRandSteine)
+		rand_Steine = (100.0 * gegRandSteine)/(eigRandSteine + gegRandSteine);
+    else
+        rand_Steine = 0;
+    end % if (eigRandSteine > gegRandSteine)
+    
+    % Eckfelder berechnen
+    eigSteine = 0;
+    gegSteine = 0;
+	if(board(1,1) == color) 
+        eigSteine = eigSteine +1;
+    elseif(board(1,1) == -color) 
+        gegSteine = gegSteine+1;
     end
+	if(board(1,8) == color) 
+        eigSteine = eigSteine+1;
+    elseif(board(1,8) == -color) 
+        gegSteine = gegSteine+1;
+    end
+	if(board(8,1) == color) 
+        eigSteine = eigSteine+1;
+    elseif(board(8,1) == +color) 
+        gegSteine = gegSteine+1;
+    end
+	if(board(8,8) == color) 
+        eigSteine = eigSteine+1;
+    elseif(board(8,8) == -color) 
+        gegSteine = gegSteine+1;
+    end
+    
+	corners = 25 * (eigSteine - gegSteine);
+
+    % corner closeness
+	eigSteine = 0;
+    gegSteine = 0;
+    
+	if(board(1,1) == 0)
+		if(board(1,2) == color) 
+            eigSteine = eigSteine+1;
+        elseif(board(1,2) == -color) 
+            gegSteine = gegSteine+1;
+        end % if(board(1,2) == color) 
+		if(board(2,2) == color) 
+            eigSteine = eigSteine+1;
+        elseif(board(2,2) == -color) 
+            gegSteine = gegSteine+1;
+        end % if(board(2,2) == color) 
+		if(board(2,1) == color) 
+            eigSteine = eigSteine+1;
+        elseif(board(2,1) == -color) 
+            gegSteine = gegSteine+1;
+        end %if(board(2,1) == color)
+    end % if (board(1,1) == 0)
+    
+    if(board(1,8) == 0)
+		if(board(1,7) == color) 
+            eigSteine = eigSteine+1;
+        elseif(board(1,7) == -color) 
+            gegSteine = gegSteine+1;
+        end % if(board(1,7) == color) 
+		if(board(2,7) == color) 
+            eigSteine = eigSteine+1;
+        elseif(board(2,7) == -color) 
+            gegSteine = gegSteine+1;
+        end % if(board(2,7) == color) 
+		if(board(2,8) == color) 
+            eigSteine = eigSteine+1;
+        elseif(board(2,8) == -color) 
+            gegSteine = gegSteine+1;
+        end %if(board(2,8) == color)
+    end % if (board(1,8) == 0)
+    
+    if(board(8,1) == 0)
+		if(board(8,2) == color) 
+            eigSteine = eigSteine+1;
+        elseif(board(8,2) == -color) 
+            gegSteine = gegSteine+1;
+        end % if(board(8,2) == color) 
+		if(board(7,2) == color) 
+            eigSteine = eigSteine+1;
+        elseif(board(7,2) == -color) 
+            gegSteine = gegSteine+1;
+        end % if(board(7,2) == color) 
+		if(board(7,1) == color) 
+            eigSteine = eigSteine+1;
+        elseif(board(7,1) == -color) 
+            gegSteine = gegSteine+1;
+        end %if(board(7,1) == color)
+    end % if (board(8,1) == 0)
+
+    if(board(8,8) == 0)
+		if(board(7,8) == color) 
+            eigSteine = eigSteine+1;
+        elseif(board(7,8) == -color) 
+            gegSteine = gegSteine+1;
+        end % if(board(8,8) == color) 
+		if(board(7,7) == color) 
+            eigSteine = eigSteine+1;
+        elseif(board(7,7) == -color) 
+            gegSteine = gegSteine+1;
+        end % if(board(7,7) == color) 
+		if(board(8,7) == color) 
+            eigSteine = eigSteine+1;
+        elseif(board(8,7) == -color) 
+            gegSteine = gegSteine+1;
+        end %if(board(8,7) == color)
+    end % if (board(8,8) == 0)    
+    
+	closeness = -12.5 * (eigSteine - gegSteine);
+
+%     % Mobilitaet berechnen
+% 	eigSteine = num_valid_moves(my_color, opp_color, board);
+% 	gegSteine = num_valid_moves(opp_color, my_color, board);
+% 	if(eigSteine > gegSteine)
+% 		mobility = (100.0 * eigSteine)/(eigSteine + gegSteine);
+%     elseif(eigSteine < gegSteine)
+% 		mobility = -(100.0 * gegSteine)/(eigSteine + gegSteine);
+%     else
+%         moblity = 0;
+%     end % if(eigSteine > gegSteine)
+mobility = 0;
+    
+    score = (10 * piece_diff) + (801.724 * corners) + (382.026 * closeness) + (78.922 * mobility)  + (74.396 * rand_Steine) + (10 * value);
 end
