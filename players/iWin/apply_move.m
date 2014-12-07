99d1cb7e9ca13a4db199458c25a6c76accd6d71a
Christiane Sandner <christianesandner@r204129.local>
2014-12-07 17:27:56 +0100

    Geschwindigkeitsoptimierung von apply_move (40% schneller)

--- players/iWin/apply_move.m
+++ players/iWin/apply_move.m
@@ -6,18 +6,18 @@ function [ b ] = apply_move( b, color, pos )
     % alle moeglichen Richtungen
     v  = [-1, 0; -1, 1; 0, 1; 1, 1; 1, 0; 1, -1; 0, -1; -1, -1];
 
-    r = 1;
     move_valid = 0;
 
     % falls Feld frei
     if b(pos(1), pos(2)) == 0
         % schaue in alle 8 Richtungen nach der Farbe des Gegners
         for l = 1:8
-            pos2 = pos;
-            pos2 = pos2 + v(l,:);
+            pos2 = pos + v(l,:);
 
             % Gehe nicht über das Spielfeld hinaus
             if (pos2(1) < 1) || (pos2(2) < 1) || (pos2(1) > 8) || (pos2(2) > 8)
+            %if any(pos2(1) == [0, 9] | pos2(2) == [0, 9])
+            %if min(pos2) == 0 || max(pos2) == 9
                 continue;
 
             % falls Farbe des Gegeners
@@ -25,26 +25,24 @@ function [ b ] = apply_move( b, color, pos )
 
                 % gehe in aktuelle Richtung weiter
                 for s = 2:7
-                    pos3 = pos;
-
-                    pos3 = pos3 + s*v(l,:);
-                    if pos3(1) < 1 || pos3(2) < 1 || pos3(1) > 8 || pos3(2) > 8
-                        continue;
+                    pos2 = pos2 + v(l,:);
+                    if pos2(1) < 1 || pos2(2) < 1 || pos2(1) > 8 || pos2(2) > 8
+                    %if any(pos2(1) == [0, 9] | pos2(2) == [0, 9])
+                    %if min(pos2) == 0 || max(pos2) == 9
+                        break;
 
                     % sobald Feld = eigene Farbe ist Zug gueltig
-                    elseif b(pos3(1), pos3(2)) == color
+                    elseif b(pos2(1), pos2(2)) == color
 
                         % Drehe alle Steine fuer aktuelle Richtung um
                         for t = 0:s - 1
                             pos4 = pos + t*v(l,:);
                             b(pos4(1), pos4(2)) = color;
-                            move_valid = 1;
                         end
-
-                        r = r + 1;
+                        move_valid = 1;
                         break;
 
-                    elseif b(pos3(1), pos3(2)) == 0
+                    elseif b(pos2(1), pos2(2)) == 0
                         break;
                     end
                 end
