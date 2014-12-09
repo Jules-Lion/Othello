fb75c06a911867db3f7069cdeaa52d87c1bc682e
Christiane Sandner <christianesandner@r204129.local>
2014-12-09 22:40:57 +0100

    get_valid_moves und kalibrierung ausgebessert

--- kalibrierung.m
+++ kalibrierung.m
@@ -300,71 +300,64 @@ end
 
 function [ valid_moves, boards ] = get_valid_moves( board, color )
     % alle moeglichen Richtungen
-    v  = [-1, 0; -1, 1; 0, 1; 1, 1; 1, 0; 1, -1; 0, -1; -1, -1];
-    
+    v = [-1, -1,  0,  1,  1,  1,  0, -1;
+          0,  1,  1,  1,  0, -1, -1, -1];
+
     valid_moves = [];
     boards = {};
 
-    for i = 1:8
-        for j = 1:8
-            b = board;
-            pos = [i, j];
-            
-            % Diese Funktion gibt die neue Spielsituation für alle moeglichen Zuege
-            % zurueck.
+    [row, col] = find(conv2(double(board == -color), ones(3), 'same') > 0 & board == 0);
+    
+    for pos = [row'; col']
+        b = board;
+        move_valid = 0;
 
-            move_valid = 0;
+        % falls Feld frei
+        if b(pos(1), pos(2)) == 0
+            % schaue in alle 8 Richtungen nach der Farbe des Gegners
+            for l = 1:8
+                pos2 = pos + v(:,l);
 
-            % falls Feld frei
-            if b(pos(1), pos(2)) == 0
-                % schaue in alle 8 Richtungen nach der Farbe des Gegners
-                for l = 1:8
-                    pos2 = pos + v(l,:);
+                % Gehe nicht über das Spielfeld hinaus
+                if (pos2(1) < 1) || (pos2(2) < 1) || (pos2(1) > 8) || (pos2(2) > 8)
+                    continue;
 
-                    % Gehe nicht über das Spielfeld hinaus
-                    if (pos2(1) < 1) || (pos2(2) < 1) || (pos2(1) > 8) || (pos2(2) > 8)
-                    %if any(pos2(1) == [0, 9] | pos2(2) == [0, 9])
-                    %if min(pos2) == 0 || max(pos2) == 9
-                        continue;
+                % falls Farbe des Gegeners
+                elseif b(pos2(1), pos2(2)) == -color
 
-                    % falls Farbe des Gegeners
-                    elseif b(pos2(1), pos2(2)) == -color
+                    % gehe in aktuelle Richtung weiter
+                    for s = 2:7
+                        pos2 = pos2 + v(:,l);
+                        if pos2(1) < 1 || pos2(2) < 1 || pos2(1) > 8 || pos2(2) > 8
+                            break;
 
-                        % gehe in aktuelle Richtung weiter
-                        for s = 2:7
-                            pos2 = pos2 + v(l,:);
-                            if pos2(1) < 1 || pos2(2) < 1 || pos2(1) > 8 || pos2(2) > 8
-                            %if any(pos2(1) == [0, 9] | pos2(2) == [0, 9])
-                            %if min(pos2) == 0 || max(pos2) == 9
-                                break;
+                        % sobald Feld = eigene Farbe ist Zug gueltig
+                        elseif b(pos2(1), pos2(2)) == color
 
-                            % sobald Feld = eigene Farbe ist Zug gueltig
-                            elseif b(pos2(1), pos2(2)) == color
-
-                                % Drehe alle Steine fuer aktuelle Richtung um
-                                for t = 0:s - 1
-                                    pos4 = pos + t*v(l,:);
-                                    b(pos4(1), pos4(2)) = color;
-                                end
-                                move_valid = 1;
-                                break;
-
-                            elseif b(pos2(1), pos2(2)) == 0
-                                break;
+                            % Drehe alle Steine fuer aktuelle Richtung um
+                            for t = 0:s - 1
+                                pos4 = pos + t*v(:,l);
+                                b(pos4(1), pos4(2)) = color;
                             end
+                            move_valid = 1;
+                            break;
+
+                        elseif b(pos2(1), pos2(2)) == 0
+                            break;
                         end
                     end
                 end
             end
+        end
 
-            % merke gueltigen Zug
-            if move_valid == 1
-                valid_moves(end + 1, :) = pos;
-                boards{end + 1} = b;
-            end
+        % merke gueltigen Zug
+        if move_valid == 1
+            valid_moves(end + 1, :) = pos;
+            boards{end + 1} = b;
         end
     end
 end
+
 
 function [max_v, b] = WeWinMagic(board, depth, color, alpha, beta, anfangstiefe)
 
@@ -372,24 +365,32 @@ maxDepth = anfangstiefe;
 max_value = alpha;
 
 
+% % hier die Nuller pruefen number of non zero elements in matrix
+% if (nnz(board)<10)
+%     
+%     b = get_first_boards(board);
+%     max_v=0;
+% 
+% else
+
 [ moves_list, boards ] = get_valid_moves(board, color);
 
 num_moves = size(moves_list, 1);
 % TESTING
-if (depth == maxDepth)
-   if (num_moves <= 2)
-       depth = depth + 2;
-       maxDepth = maxDepth + 2;
-   elseif (num_moves <= 5)
-       depth = depth + 1;
-       maxDepth = maxDepth + 1;
-   elseif (num_moves <= 10)
-       % do nothing
-   else 
-       depth = depth - 1;
-       maxDepth = maxDepth - 1;
-   end
-end
+% if (depth == maxDepth)
+%    if (num_moves <= 1)
+%        depth = depth + 1;
+%        maxDepth = maxDepth + 2;
+%    elseif (num_moves <= 5)
+%        depth = depth + 1;
+%        maxDepth = maxDepth + 1;
+%    elseif (num_moves <= 10)
+%         maxDepth = maxDepth - 1;
+%    else 
+%        depth = depth - 3;
+%        maxDepth = maxDepth - 3;
+%    end
+% end
 % TESTING
 
 if depth <= 0
@@ -419,6 +420,8 @@ else
     max_v = max_value;
 end
 
+% end   %end fuer if find board
+
 end
 
 function [min_v]  = min(color, depth, board, alpha, beta, anfangstiefe)
@@ -430,7 +433,7 @@ min_value = beta;
 
 num_moves = size(moves_list, 1);
 
-if depth == 0
+if depth <= 0
     min_v = evaluation(board, color, moves_list);
 elseif num_moves == 0
     min_v = WeWinMagic(board, depth-1, -color, alpha, min_value, maxDepth);
@@ -452,4 +455,10 @@ else
     min_v = min_value;
 end
 
+
+
 end
+
+
+
+
--- players/iWin/get_valid_moves.m
+++ players/iWin/get_valid_moves.m
@@ -1,67 +1,59 @@
 function [ valid_moves, boards ] = get_valid_moves( board, color )
     % alle moeglichen Richtungen
-    v  = [-1, 0; -1, 1; 0, 1; 1, 1; 1, 0; 1, -1; 0, -1; -1, -1];
-    
+    v = [-1, -1,  0,  1,  1,  1,  0, -1;
+          0,  1,  1,  1,  0, -1, -1, -1];
+
     valid_moves = [];
     boards = {};
 
-    for i = 1:8
-        for j = 1:8
-            b = board;
-            pos = [i, j];
-            
-            % Diese Funktion gibt die neue Spielsituation für alle moeglichen Zuege
-            % zurueck.
+    [row, col] = find(conv2(double(board == -color), ones(3), 'same') > 0 & board == 0);
+    
+    for pos = [row'; col']
+        b = board;
+        move_valid = 0;
 
-            move_valid = 0;
+        % falls Feld frei
+        if b(pos(1), pos(2)) == 0
+            % schaue in alle 8 Richtungen nach der Farbe des Gegners
+            for l = 1:8
+                pos2 = pos + v(:,l);
 
-            % falls Feld frei
-            if b(pos(1), pos(2)) == 0
-                % schaue in alle 8 Richtungen nach der Farbe des Gegners
-                for l = 1:8
-                    pos2 = pos + v(l,:);
+                % Gehe nicht über das Spielfeld hinaus
+                if (pos2(1) < 1) || (pos2(2) < 1) || (pos2(1) > 8) || (pos2(2) > 8)
+                    continue;
 
-                    % Gehe nicht über das Spielfeld hinaus
-                    if (pos2(1) < 1) || (pos2(2) < 1) || (pos2(1) > 8) || (pos2(2) > 8)
-                    %if any(pos2(1) == [0, 9] | pos2(2) == [0, 9])
-                    %if min(pos2) == 0 || max(pos2) == 9
-                        continue;
+                % falls Farbe des Gegeners
+                elseif b(pos2(1), pos2(2)) == -color
 
-                    % falls Farbe des Gegeners
-                    elseif b(pos2(1), pos2(2)) == -color
+                    % gehe in aktuelle Richtung weiter
+                    for s = 2:7
+                        pos2 = pos2 + v(:,l);
+                        if pos2(1) < 1 || pos2(2) < 1 || pos2(1) > 8 || pos2(2) > 8
+                            break;
 
-                        % gehe in aktuelle Richtung weiter
-                        for s = 2:7
-                            pos2 = pos2 + v(l,:);
-                            if pos2(1) < 1 || pos2(2) < 1 || pos2(1) > 8 || pos2(2) > 8
-                            %if any(pos2(1) == [0, 9] | pos2(2) == [0, 9])
-                            %if min(pos2) == 0 || max(pos2) == 9
-                                break;
+                        % sobald Feld = eigene Farbe ist Zug gueltig
+                        elseif b(pos2(1), pos2(2)) == color
 
-                            % sobald Feld = eigene Farbe ist Zug gueltig
-                            elseif b(pos2(1), pos2(2)) == color
-
-                                % Drehe alle Steine fuer aktuelle Richtung um
-                                for t = 0:s - 1
-                                    pos4 = pos + t*v(l,:);
-                                    b(pos4(1), pos4(2)) = color;
-                                end
-                                move_valid = 1;
-                                break;
-
-                            elseif b(pos2(1), pos2(2)) == 0
-                                break;
+                            % Drehe alle Steine fuer aktuelle Richtung um
+                            for t = 0:s - 1
+                                pos4 = pos + t*v(:,l);
+                                b(pos4(1), pos4(2)) = color;
                             end
+                            move_valid = 1;
+                            break;
+
+                        elseif b(pos2(1), pos2(2)) == 0
+                            break;
                         end
                     end
                 end
             end
+        end
 
-            % merke gueltigen Zug
-            if move_valid == 1
-                valid_moves(end + 1, :) = pos;
-                boards{end + 1} = b;
-            end
+        % merke gueltigen Zug
+        if move_valid == 1
+            valid_moves(end + 1, :) = pos;
+            boards{end + 1} = b;
         end
     end
 end
